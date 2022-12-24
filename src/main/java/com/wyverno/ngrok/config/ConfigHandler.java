8a2b624ee091a06c0e04f4f6b30d178c7d132236
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-12-24 16:22:51 +0200

    Add java class ConfigHandler

--- src/main/java/com/wyverno/ngrok/config/ConfigHandler.java
+++ src/main/java/com/wyverno/ngrok/config/ConfigHandler.java
@@ -1,2 +1,10 @@
-package com.wyverno.ngrok.config;public class ConfigHandler {
+package com.wyverno.ngrok.config;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ConfigHandler {
+    public ConfigHandler() {
+        System.out.println(Files.exists(Paths.get("config")));
+    }
 }
