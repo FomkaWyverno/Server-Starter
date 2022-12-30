cbbc98dea6983e2ffb19b0645310459940e8e4e9
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-12-30 17:51:22 +0200

    Add command '/stop' for to stop correct this program

--- src/main/java/com/wyverno/Main.java
+++ src/main/java/com/wyverno/Main.java
@@ -2,22 +2,40 @@ package com.wyverno;
 
 import com.wyverno.ngrok.ErrorInNgrokProcessException;
 import com.wyverno.ngrok.Ngrok;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Main {
 
 
+    private volatile static String line = "";
+    private volatile static Ngrok ngrok;
     public static void main(String[] args) throws IOException, InterruptedException {
-        while (true) {
-            try {
-                Ngrok ngrok = new Ngrok(Paths.get("config\\config.properties"));
-                ngrok.start();
-                System.out.println(ngrok);
-                ngrok.join();
-            } catch (ErrorInNgrokProcessException e) {
-                System.out.println("Ngrok restart");
-            }
-        }
+         Thread threadNgrok = new Thread(() -> {
+             while (!line.equals("/stop")) {
+                 try {
+                     ngrok = new Ngrok(Paths.get("config\\config.properties"));
+                     ngrok.start();
+                     System.out.println(ngrok);
+                     ngrok.join();
+                 } catch (ErrorInNgrokProcessException e) {
+                     System.out.println("Ngrok restart");
+                 } catch (IOException | InterruptedException e) {
+                     e.printStackTrace();
+                 }
+             }
+         });
+         threadNgrok.start();
+         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+             while ((line = reader.readLine()) != null) {
+                 if (line.equals("/stop")) {
+                     ngrok.interrupt();
+                     break;
+                 }
+             }
+         }
     }
 }
