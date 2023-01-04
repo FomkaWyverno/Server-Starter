1b74ae30aed7b84849d257613d979853cafbed54
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-01-04 20:40:35 +0200

    Program will be stop if NgrokProcess is launched

--- src/main/java/com/wyverno/Main.java
+++ src/main/java/com/wyverno/Main.java
@@ -14,30 +14,44 @@ public class Main {
 
     private volatile static String line = "";
     private volatile static Ngrok ngrok;
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         ConfigHandler configHandler = new ConfigHandler(Paths.get("config\\config.properties"), Ngrok.class);
-         Thread threadNgrok = new Thread(() -> {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Thread threadNgrok = new Thread(() -> {
              while (!line.equals("/stop")) {
                  try {
                      ngrok = new Ngrok(configHandler);
                      ngrok.start();
                      System.out.println(ngrok);
                      ngrok.join();
-                 } catch (ErrorInNgrokProcessException e) {
-                     System.out.println("Ngrok restart");
+
+                     if (ngrok.isOtherProcessLaunched()) {
+                         reader.close();
+                         break;
+                     } else if (!line.equals("/stop")) {
+                         System.out.println("Ngrok restart");
+                     } else {
+                         System.out.println("Program is stop");
+                     }
                  } catch (IOException | InterruptedException e) {
                      e.printStackTrace();
                  }
              }
          });
          threadNgrok.start();
-         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-             while ((line = reader.readLine()) != null) {
-                 if (line.equals("/stop")) {
-                     ngrok.interrupt();
-                     break;
+         try {
+             while (!line.equals("/stop")) {
+                 if (reader.ready()) {
+                     line = reader.readLine();
+                     if (line.equals("/stop")) {
+                         ngrok.interrupt();
+                         break;
+                     }
                  }
              }
+             reader.close();
+         } catch (IOException e) {
+             System.out.println("Program is stop");
          }
     }
 }
