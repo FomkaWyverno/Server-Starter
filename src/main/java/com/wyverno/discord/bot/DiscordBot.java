b7b8f495a6bcadf076d59d21cb4e456c1cc105ac
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-01-07 13:09:24 +0200

    Add discord bot for notify users about public ip

--- src/main/java/com/wyverno/discord/bot/DiscordBot.java
+++ src/main/java/com/wyverno/discord/bot/DiscordBot.java
@@ -1,2 +1,59 @@
-package com.wyverno.discord.bot;public class DiscordBot {
+package com.wyverno.discord.bot;
+
+import com.wyverno.config.Config;
+import com.wyverno.config.ConfigHandler;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscordBot extends Thread {
+
+    @Config(comment = "Token bot for authorization")
+    private String TOKEN;
+
+    @Config(comment = "Id text channel where bot will be send message with ip")
+    private String TEXT_CHANNEL_ID;
+
+    private final ConfigHandler configHandler;
+    private final String TEXT_MESSAGE;
+
+    public DiscordBot(ConfigHandler configHandler, String textMessage) {
+        this.configHandler = configHandler;
+        this.TEXT_MESSAGE = textMessage;
+
+        List<Field> configFields = new ArrayList<>();
+
+        for (Field field : this.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(Config.class)) {
+                configFields.add(field);
+            }
+        }
+
+        configFields.forEach(field -> {
+            String property = this.configHandler.getProperty(field.getName().toLowerCase());
+            try {
+                field.set(this, property);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this);
+    }
+
+    private synchronized void fixSelfConfig(DiscordBotError... discordBotErrors) {
+        this.configHandler.fixConfig(discordBotErrors);
+    }
+
+    @Override
+    public String toString() {
+        return "DiscordBot{" +
+                "TOKEN='" + TOKEN + '\'' +
+                ", TEXT_CHANNEL_ID='" + TEXT_CHANNEL_ID + '\'' +
+                ", TEXT_MESSAGE='" + TEXT_MESSAGE + '\'' +
+                '}';
+    }
 }
