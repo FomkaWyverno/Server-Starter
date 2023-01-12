fd71d082a811f518ebc66f4c848b54c1f09eb407
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-01-12 15:09:47 +0200

    Field for need notify Discord

--- src/main/java/com/wyverno/Main.java
+++ src/main/java/com/wyverno/Main.java
@@ -1,5 +1,7 @@
 package com.wyverno;
 
+import com.wyverno.config.Config;
+import com.wyverno.discord.bot.DiscordBot;
 import com.wyverno.ngrok.Ngrok;
 import com.wyverno.config.ConfigHandler;
 
@@ -15,8 +17,13 @@ public class Main {
     private volatile static Ngrok ngrok;
     private static final Object lockNgrok = new Object();
 
+    @Config(comment = "Need notify text channel in discord? default = true")
+    public static boolean notify_Discord = false;
+
     public static void main(String[] args) throws IOException {
-        ConfigHandler configHandler = new ConfigHandler(Paths.get("config\\config.properties"), Ngrok.class);
+        ConfigHandler configHandler = new ConfigHandler(Paths.get("config\\config.properties"), Ngrok.class, Main.class, DiscordBot.class);
+        notify_Discord = Boolean.parseBoolean(configHandler.getProperty("notify_discord"));
+        System.out.printf("notify_Discord = %b%n", notify_Discord);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Thread threadNgrok = new Thread(() -> {
              while (!line.equals("/stop")) {
@@ -46,10 +53,15 @@ public class Main {
 
          Thread threadIP = new Thread(() -> {
              while (!line.equals("/stop")) {
-                 Ngrok aliveNgrok = getNgrok();
+                 Ngrok aliveNgrok =  getNgrok();
                  try {
                      String publicURL = aliveNgrok.getTunnel().getPublic_url();
                      System.out.println("PUBLIC URL >>> " + publicURL);
+                     if (notify_Discord) {
+                         System.out.println("Notify Discord");
+                     } else {
+                         System.out.println("Not need notify discord");
+                     }
                      break;
                  } catch (NullPointerException e) {
                      System.out.println("Ngrok is null");
@@ -80,6 +92,7 @@ public class Main {
         try {
             synchronized (lockNgrok) {
                 while (ngrok == null || !ngrok.isAliveNgrok()) {
+                    System.out.printf("ngrok == null = %b || !ngrok.isAliveNgrok() = %b%n", ngrok == null, ngrok != null && !ngrok.isAliveNgrok());
                     System.out.println("Wait getNgrok()");
                     lockNgrok.wait();
                     System.out.println("Notify getNgrok()");
