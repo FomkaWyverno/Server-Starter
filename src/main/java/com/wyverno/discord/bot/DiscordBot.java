f282c48458905516502169386a7dda3d09608dc3
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-01-12 17:01:54 +0200

    Sending message in text channel in discord and selfConfigFix

--- src/main/java/com/wyverno/discord/bot/DiscordBot.java
+++ src/main/java/com/wyverno/discord/bot/DiscordBot.java
@@ -2,11 +2,17 @@ package com.wyverno.discord.bot;
 
 import com.wyverno.config.Config;
 import com.wyverno.config.ConfigHandler;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DiscordBot extends Thread {
+
+    private JDA jda;
 
     @Config(comment = "Token bot for authorization")
     private String TOKEN;
@@ -17,6 +23,7 @@ public class DiscordBot extends Thread {
     private final ConfigHandler configHandler;
     private final String TEXT_MESSAGE;
 
+    private boolean isMessageSent = false;
     public DiscordBot(ConfigHandler configHandler, String textMessage) {
         this.configHandler = configHandler;
         this.TEXT_MESSAGE = textMessage;
@@ -41,19 +48,65 @@ public class DiscordBot extends Thread {
 
     @Override
     public void run() {
-        System.out.println(this);
+        this.runProcess();
+    }
+
+    private void runProcess() {
+        try {
+            System.out.println(this);
+            runDiscordBot();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.err.println("Text ID Channel may be only Number!");
+            this.fixSelfConfig(DiscordBotError.InvalidIDTextChat);
+            this.shutdown();
+        } catch (IllegalArgumentException e) {
+            if (e.getMessage().equals("Token may not be empty")) {
+                System.err.println("Discord Bot token is missing exception");
+                this.fixSelfConfig(DiscordBotError.InvalidToken);
+            } else if (e.getMessage().equals("ID may not be empty")) {
+                System.err.println("Discord Bot Id Text Channel not be empty");
+                this.fixSelfConfig(DiscordBotError.InvalidIDTextChat);
+            } else {
+                System.err.println("Other problem");
+                e.printStackTrace();
+            }
+            this.shutdown();
+        }
+    }
+
+    private void runDiscordBot() throws InterruptedException {
+        this.jda = JDABuilder.createLight(this.TOKEN).build();
+        TextChannel textChannel = jda.awaitReady().getTextChannelById(TEXT_CHANNEL_ID);
+        if (textChannel != null) {
+            textChannel.sendMessage(TEXT_MESSAGE).queue();
+            this.isMessageSent = true;
+        } else {
+            this.fixSelfConfig(DiscordBotError.InvalidIDTextChat);
+        }
+
+        this.shutdown();
     }
 
     private synchronized void fixSelfConfig(DiscordBotError... discordBotErrors) {
         this.configHandler.fixConfig(discordBotErrors);
     }
 
+    public boolean isMessageSent() {
+        return isMessageSent;
+    }
+
+    public void shutdown() {
+        this.jda.shutdown();
+    }
+
     @Override
     public String toString() {
-        return "DiscordBot{" +
+        return "DiscordBot {\n" +
                 "TOKEN='" + TOKEN + '\'' +
-                ", TEXT_CHANNEL_ID='" + TEXT_CHANNEL_ID + '\'' +
-                ", TEXT_MESSAGE='" + TEXT_MESSAGE + '\'' +
+                ",\nTEXT_CHANNEL_ID='" + TEXT_CHANNEL_ID + '\'' +
+                ",\nTEXT_MESSAGE='" + TEXT_MESSAGE + '\'' +
                 '}';
     }
 }
